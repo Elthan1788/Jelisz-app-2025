6d3b46130c47fa7e17b13fbe446acd8de30d562d
Elthan1788 <kimchun25390@gmail.com>
2025-08-17 22:25:51 +0800

    Create index.js

---
+++ public/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App'; // 导入您的 App 组件
+
+// Create a root to render the React application
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);Add src/index.js
