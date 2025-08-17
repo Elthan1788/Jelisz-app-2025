7e004d6c7538ca245995c1e946d1e506b985634b
Elthan1788 <kimchun25390@gmail.com>
2025-08-17 23:48:30 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App'; // 导入您的 App 组件
+import './index.css'; // 确保这里正确导入了 Tailwind CSS 的主文件
 
 // Create a root to render the React application
 const root = ReactDOM.createRoot(document.getElementById('root'));
